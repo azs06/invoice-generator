e5d98e28cee08f7273aca14d401ae7dc5690de09
Md. Atiquzzaman Soikat <atik.aiub@gmail.com>
2025-11-26 16:35:34 +0600

    updated build config

--- svelte.config.js
+++ svelte.config.js
@@ -7,17 +7,9 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			// See below for an explanation of these options
-			config: undefined,
 			platformProxy: {
 				configPath: 'wrangler.toml',
-				environment: undefined,
 				persist: '.wrangler/state'
-			},
-			fallback: 'plaintext',
-			routes: {
-				include: ['/*'],
-				exclude: ['<all>']
 			}
 		}),
 		alias: {
